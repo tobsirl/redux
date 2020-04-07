3295f8ab1c52583b0dea7f84da9a8227c2938aeb
Paul Tobin <tobster111@gmail.com>
2020-04-07 12:14:30 +0100

    Added a decrement action and updated the reducer

--- Plain-JS-Redux/redux.js
+++ Plain-JS-Redux/redux.js
@@ -1,5 +1,32 @@
 const redux = require('redux');
 
-const action = {
-  type: 'INCREMENT',
-};
+function increment() {
+  return {
+    type: 'INCREMENT',
+  };
+}
+
+function decrement() {
+  return {
+    type: 'DECREMENT',
+  };
+}
+
+function reducer(state = { count: 0 }, action) {
+  // return new state based on the incoming action.type
+  if (action.type === 'INCREMENT') {
+    return {
+      count: state.count + 1,
+    };
+  } else if (action.type === 'DECREMENT') {
+    return {
+      count: state.count - 1,
+    };
+  }
+}
+
+const store = redux.createStore()
+
+
+console.log(increment());
+

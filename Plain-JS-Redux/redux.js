a1fd594e052bfd6fc0395d87b56fe2e4f78abdbc
Paul Tobin <tobster111@gmail.com>
2020-04-07 12:41:16 +0100

    Added two more actions double and halve using Math.floor()

--- Plain-JS-Redux/redux.js
+++ Plain-JS-Redux/redux.js
@@ -13,7 +13,19 @@ function decrement() {
   };
 }
 
-const reducer = (state = { count: 0 }, action) => {
+function double() {
+  return {
+    type: 'DOUBLE',
+  };
+}
+
+function halve() {
+  return {
+    type: 'HALVE',
+  };
+}
+
+function reducer(state = { count: 0 }, action) {
   // return new state based on the incoming action.type
 
   switch (action.type) {
@@ -25,15 +37,19 @@ const reducer = (state = { count: 0 }, action) => {
       return {
         count: state.count - 1,
       };
+    case 'DOUBLE':
+      return {
+        count: state.count * 2,
+      };
+    case 'HALVE':
+      return {
+        count: Math.round(state.count / 2),
+      };
     default:
       return state;
   }
-};
-
-
+}
 
 const store = createStore(reducer);
 
-store.
-
-console.log(increment());
+store.console.log(increment());

7932f2c654de4affe48c8033244d68706e5fb79f
Paul Tobin <tobster111@gmail.com>
2020-04-09 16:45:25 +0100

    Create a rootReducer that uses combineReducers to create a state tree

--- Plain-JS-Redux/redux/index.js
+++ Plain-JS-Redux/redux/index.js
@@ -7,5 +7,11 @@ import favoriteThingsReducer from './favoriteThings';
 import youtubeVideoReducer from './youtubeVideos';
 
 // combine the reducers into a single state tree
+const rootReducer = combineReducers({
+  count: countReducer,
+  favoriteThings: favoriteThingsReducer,
+  youtubeVideo: youtubeVideoReducer,
+});
+
 // create the store
 // export the store

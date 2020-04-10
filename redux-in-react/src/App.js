162d0ce6429ade40b5a414f10ba799bde48ef5aa
Paul Tobin <tobster111@gmail.com>
2020-04-10 12:21:57 +0100

    Import connect from react-redux, use it to call the component

--- redux-in-react/src/App.js
+++ redux-in-react/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import './App.css';
 
 function App() {
@@ -9,4 +10,4 @@ function App() {
   );
 }
 
-export default App;
+export default connect(/* What parts of state do you want? , What actions to dispatch */)(App);

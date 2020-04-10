fd0545f2fb8965620a454c718b764cfc7d26864f
Paul Tobin <tobster111@gmail.com>
2020-04-10 11:23:54 +0100

    Setup action, reducer and created the store, subsribed to it and exported as a default

--- redux-in-react/src/redux/index.js
+++ redux-in-react/src/redux/index.js
@@ -1,4 +1,33 @@
 import redux, { createStore } from 'redux';
 
 // actions
+function increment() {
+  return {
+    type: 'INCREMENT',
+  };
+}
 
+function decrement() {
+  return {
+    type: 'DECREMENT',
+  };
+}
+
+function reducer(count = 0, action) {
+  switch (action.type) {
+    case 'INCREMENT':
+      return count + 1;
+    case 'DECREMENT':
+      return count - 1;
+    default:
+      return count;
+  }
+}
+
+const store = createStore(reducer);
+
+store.subscribe(() => {
+  console.log(store.getState);
+});
+
+export default store;

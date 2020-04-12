f648abf51ebf9737b57e86d4ee86e456d8894bc3
Paul Tobin <tobster111@gmail.com>
2020-04-12 12:05:59 +0100

    Using useDispatch()

--- redux-in-react/src/App.js
+++ redux-in-react/src/App.js
@@ -1,18 +1,19 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { increment, decrement } from './redux';
 import './App.css';
 
 function App() {
   const count = useSelector((state) => state);
-  
+  const dispatch = useDispatch();
+
   return (
     <div className="App">
       <h1>{count}</h1>
-      <button className="btn" onClick={increment}>
+      <button className="btn" onClick={() => dispatch(increment())}>
         +
       </button>
-      <button className="btn" onClick={decrement}>
+      <button className="btn" onClick={() => dispatch(decrement())}>
         -
       </button>
     </div>

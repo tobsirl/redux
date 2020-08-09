014ac8242f4c0ca926fabc40f956ef1bef114729
Paul Tobin <tobster111@gmail.com>
2020-08-09 15:41:04 +0100

    Store - Adding constants instead of comparing strings

--- store/index.js
+++ store/index.js
@@ -1,38 +1,3 @@
-function todos(state = [], action) {
-  switch (action.type) {
-    case 'ADD_TODO':
-      return state.concat([action.todo]);
-    case 'REMOVE_TODO':
-      return state.filter((todo) => todo.id !== action.id);
-    case 'TOGGLE_TODO':
-      return state.map((todo) =>
-        todo.id !== action.id
-          ? todo
-          : Object.assign({}, todo, { complete: !todo.complete })
-      );
-    default:
-      return state;
-  }
-}
-
-function goals(state = [], action) {
-  switch (action.type) {
-    case 'ADD_GOAL':
-      return state.concat([action.goal]);
-    case 'REMOVE_GOAL':
-      return state.filter((goal) => goal.id !== action.id);
-    default:
-      return state;
-  }
-}
-
-function app(state = {}, action) {
-  return {
-    todos: todos(state.todos, action),
-    goals: goals(state.goals, action),
-  };
-}
-
 /*
   Characteristics of a Pure Function
   1) They always return the same result if the same agruments are passed in.
@@ -40,6 +5,7 @@ function app(state = {}, action) {
   3) Never produce any side effects. 
 */
 
+// Library code
 function createStore(app) {
   // The store should have four parts
   // 1. The state
@@ -71,6 +37,50 @@ function createStore(app) {
     getState,
     subscribe,
     dispatch,
+  };
+}
+
+// App Code
+const ADD_TODO = 'ADD_TODO';
+const REMOVE_TODO = 'REMOVE_TODO';
+const TOGGLE_TODO = 'TOGGLE_TODO';
+const ADD_GOAL = 'ADD_GOAL';
+const REMOVE_GOAL = 'REMOVE_GOAL';
+
+// Reducer function
+function todos(state = [], action) {
+  switch (action.type) {
+    case ADD_TODO:
+      return state.concat([action.todo]);
+    case REMOVE_TODO:
+      return state.filter((todo) => todo.id !== action.id);
+    case TOGGLE_TODO:
+      return state.map((todo) =>
+        todo.id !== action.id
+          ? todo
+          : Object.assign({}, todo, { complete: !todo.complete })
+      );
+    default:
+      return state;
+  }
+}
+
+// Reducer function
+function goals(state = [], action) {
+  switch (action.type) {
+    case ADD_GOAL:
+      return state.concat([action.goal]);
+    case REMOVE_GOAL:
+      return state.filter((goal) => goal.id !== action.id);
+    default:
+      return state;
+  }
+}
+
+function app(state = {}, action) {
+  return {
+    todos: todos(state.todos, action),
+    goals: goals(state.goals, action),
   };
 }
 

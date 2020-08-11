5883306b6662db6c26f4936fa3b1758dccfd6948
Paul Tobin <tobster111@gmail.com>
2020-08-11 15:40:50 +0100

    Removing items from the store

--- store/index.js
+++ store/index.js
@@ -131,26 +131,6 @@ store.subscribe(() => {
   goals.forEach(addGoalToDOM);
 });
 
-function addTodoToDOM(todo) {
-  const node = document.createElement('li');
-  const text = document.createTextNode(todo.name);
-  node.appendChild(text);
-  node.style.textDecoration = todo.complete ? 'line-through' : 'none';
-  node.addEventListener('click', () => {
-    store.dispatch(toggleTodoAction(todo.id));
-  });
-
-  document.getElementById('todos').appendChild(node);
-}
-
-function addGoalToDOM(goal) {
-  const node = document.createElement('li');
-  const text = document.createTextNode(goal.name);
-  node.appendChild(text);
-
-  document.getElementById('goals').appendChild(node);
-}
-
 const unsubsribe = store.subscribe(() => {
   console.log(`The new state is: `, store.getState());
 });
@@ -162,6 +142,46 @@ function generateId() {
 }
 
 // DOM code
+function createRemoveButton(onClick) {
+  const removeBtn = document.createElement('button');
+  removeBtn.innerHTML = 'X';
+  removeBtn.addEventListener('click', onClick);
+
+  return removeBtn;
+}
+
+function addTodoToDOM(todo) {
+  const node = document.createElement('li');
+  const text = document.createTextNode(todo.name);
+
+  const removeBtn = createRemoveButton(() => {
+    store.dispatch(removeTodoAction(todo.id));
+  });
+
+  node.appendChild(text);
+  node.appendChild(removeBtn);
+
+  node.style.textDecoration = todo.complete ? 'line-through' : 'none';
+  node.addEventListener('click', () => {
+    store.dispatch(toggleTodoAction(todo.id));
+  });
+
+  document.getElementById('todos').appendChild(node);
+}
+
+function addGoalToDOM(goal) {
+  const node = document.createElement('li');
+  const text = document.createTextNode(goal.name);
+
+  const removeBtn = createRemoveButton(() => {
+    store.dispatch(removeGoalAction(goal.id));
+  });
+  node.appendChild(text);
+  node.appendChild(removeBtn);
+
+  document.getElementById('goals').appendChild(node);
+}
+
 function addTodo() {
   const input = document.getElementById('todo');
   const todoList = document.getElementById('todos');
